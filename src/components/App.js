4ece70ee484440de3c7a7e0d0e9b0d65754b7dbb
Fidel Alembert Lascano Valencia <fidekof@gmail.com>
2019-09-18 22:59:55 -0500

    configurando el editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App = () => <h1>Plataforma de educacion online</h1>
+const App = () => (<h1>Plataforma de educacion online</h1>);
 
 export default App;
